9585c5534e319b882a41d109b0c40dfd880f2208
劉謹賢 <liujinxian@liujinxiande-MacBook-Pro.local>
2020-03-17 18:33:52 +0800

    import Vue, ElementUI CDN in default layout and Fix error: index.ejs copy to signup.ejs

--- routes/users/signup.js
+++ routes/users/signup.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('signup');
+  let renderData = {
+    title: "註冊"
+  }
+  res.render('signup', renderData);
 });
 
 module.exports = router;

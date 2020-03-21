a3d912d8b7d9d641270e5f7c992be90f08a6e7d9
劉謹賢 <liujinxian@liujinxiande-MacBook-Pro.local>
2020-03-22 01:43:15 +0800

    Complete SignUp and Fix some bug in controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -18,31 +18,36 @@ class UserController{
         // 確認資料都存在
         if ( req.body.name && req.body.account && req.body.password ){
             let name = req.body.name, account = req.body.account, password = req.body.password;
-            // 字串內僅能有英語或數字和 [.] [_] 
-            if ( UserVerify.justInputEngorNum(account) &&  UserVerify.justInputEngorNum(password) ){
-                // 限制字串長度 6~20
-                if ( UserVerify.wordCountLimit(account, 6, 20) &&  UserVerify.wordCountLimit(password, 6, 20) ){
-                    const hash_password = crypto.createHash('sha256').update(password).digest('base64');
-                    var save_test = new muser({
-                        name: name,
-                        account: account,
-                        password: hash_password
-                    });
-                    save_test.save(function(err){
-                        if (err) return res.json( { success: false, message: '與資料庫連結發生錯誤。' } )                        ;
-                        return res.json( { success: true } );
-                    })
+            muser.findOne({account:account}).then(function( result ){
+                if(result){
+                    return res.json( { success: false, message: '帳號已存在。' } );
+                }
+                // 字串內僅能有英語或數字和 [.] [_] 
+                if ( UserVerify.justInputEngorNum(account) &&  UserVerify.justInputEngorNum(password) ){
+                    // 限制字串長度 6~20
+                    if ( UserVerify.wordCountLimit(account, 6, 20) &&  UserVerify.wordCountLimit(password, 6, 20) ){
+                        const hash_password = crypto.createHash('sha256').update(password).digest('base64');
+                        var save_test = new muser({
+                            name: name,
+                            account: account,
+                            password: hash_password
+                        });
+                        save_test.save(function(err){
+                            if (err) return res.json( { success: false, message: '與資料庫連結發生錯誤。' } );
+                            return res.json( { success: true } );
+                        })
+                    }
+                    else{
+                        return res.json( { success: false, message: '帳號或密碼限制字串長度 6~20。' } )
+                    }
                 }
                 else{
-                    res.json( { success: false, message: '帳號或密碼限制字串長度 6~20。' } )
+                    return res.json( { success: false, message: '字串內僅能有英語或數字和 [.] [_]。' } )
                 }
-            }
-            else{
-                res.json( { success: false, message: '字串內僅能有英語或數字和 [.] [_]。' } )
-            }
+            })
         }
         else{
-            res.json( { success: false, message: '系統發生錯誤。' } )
+            return res.json( { success: false, message: '系統發生錯誤。' } )
         }
     }
 }

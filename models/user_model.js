bf499286799acbd8f768711a2758ebb32af95878
劉謹賢 <liujinxian@liujinxiande-MacBook-Pro.local>
2020-03-22 00:43:36 +0800

    Add user_model.js for mongodb

--- models/user_model.js
+++ models/user_model.js
@@ -1,5 +1,31 @@
-// import database modules
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
 
-module.exports = {
-    // functions
-}
+const userSchema = new schema({
+    name: {
+        type : String,
+        required : true
+    },
+    account: {
+        type : String,
+        required : true
+    },
+    password: {
+        type : String,
+        required : true
+    },
+    is_admin: {
+        type: Boolean,
+        default: false,
+    },
+    status: {
+        type: String,
+        default: "active"
+    },
+    date: {
+        type : Date,
+        default : Date.now
+    },
+});
+
+module.exports = user = mongoose.model("users", userSchema);

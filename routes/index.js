d3f7f29753dd0b7928dba28c443fdd5b8a749ec0
劉謹賢 <liujinxian@liujinxiande-MacBook-Pro.local>
2020-03-28 22:25:17 +0800

    Change CDN to Localhost for optimization

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   let auth = req.session.uid;
-  res.render('index', { title: 'Express', auth});
+  res.render('index', { title: '首頁', auth});
 });
 
 module.exports = router;

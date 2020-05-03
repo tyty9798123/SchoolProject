4747ff2c4400d6a5aea2c2e9a1b87d1359f48a70
劉謹賢 <tyty979812@gmail.com>
2020-05-03 21:53:20 +0800

    Init information page

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,6 @@ router.get('/', function(req, res, next) {
 router.get('/information', function(req, res, next) {
   res.render('users/information.ejs', {
     title: '變更會員資料',
-    auth: ''
   });
 })
 module.exports = router;

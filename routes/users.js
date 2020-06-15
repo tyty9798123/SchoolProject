84e89ca9ea1c9a027374fda20465deee1bfc43b4
劉謹賢 <tyty979812@gmail.com>
2020-06-15 22:40:21 +0800

    Update, update_avatar front-end

--- routes/users.js
+++ routes/users.js
@@ -23,4 +23,19 @@ router.get('/record_my_physical_health', function(req, res, next){
     title: '紀錄我的身體健康狀況'
   })
 })
+
+var multer  = require('multer')
+var upload = multer()
+
+/* update Avatar */
+router.post('/update_avatar', upload.array(), function(req, res) {
+  /* 
+    3 values
+    
+    1. avatar (Binary file)
+    2. read_name (String)
+    3. self_introduction(String)
+  */
+ console.log(req);
+})
 module.exports = router;
